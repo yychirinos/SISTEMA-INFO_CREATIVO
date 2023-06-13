2cab6861f52b77f083f0dda9dd4b9763cd27ee37
yanirachirinos3 <yanirachirinos3@gmail.com>
2023-06-12 18:51:27 -0600

    Form1

--- Sistema_Info_Creativo/Form1.Designer.vb
+++ Sistema_Info_Creativo/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FrmCompras
+Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,16 +22,9 @@ Partial Class FrmCompras
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.SuspendLayout()
-        '
-        'FrmCompras
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        components = New System.ComponentModel.Container
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(592, 388)
-        Me.Name = "FrmCompras"
-        Me.Text = "FrmCompras"
-        Me.ResumeLayout(False)
-
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Text = "Form1"
     End Sub
 End Class
