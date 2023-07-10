fdebe5721421270b45d30390d9c7070daa094aa4
Héctor Obando <obandohector555@outlook.com>
2023-07-10 16:27:07 -0600

    Elementos de interfaz agregados, campos de texto.

--- Sistema_Info_Creativo/FrmProveedores.Designer.vb
+++ Sistema_Info_Creativo/FrmProveedores.Designer.vb
@@ -22,17 +22,187 @@ Partial Class FrmProveedores
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmProveedores))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.txtBuscarTelefono = New System.Windows.Forms.TextBox()
+        Me.btnbuscar = New System.Windows.Forms.Button()
+        Me.txtBuscarNombre = New System.Windows.Forms.TextBox()
+        Me.txtBuscarID = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Location = New System.Drawing.Point(-1, -2)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(997, 57)
+        Me.Panel1.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Times New Roman", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(371, 11)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(281, 40)
+        Me.Label4.TabIndex = 25
+        Me.Label4.Text = "PROVEEDORES"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(896, 0)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(65, 66)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 24
+        Me.PictureBox3.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnLimpiar)
+        Me.GroupBox1.Controls.Add(Me.txtBuscarTelefono)
+        Me.GroupBox1.Controls.Add(Me.btnbuscar)
+        Me.GroupBox1.Controls.Add(Me.txtBuscarNombre)
+        Me.GroupBox1.Controls.Add(Me.txtBuscarID)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 77)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(950, 113)
+        Me.GroupBox1.TabIndex = 19
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Buscar por :"
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackgroundImage = CType(resources.GetObject("btnLimpiar.BackgroundImage"), System.Drawing.Image)
+        Me.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Tai Le", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLimpiar.Location = New System.Drawing.Point(877, 17)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(39, 40)
+        Me.btnLimpiar.TabIndex = 15
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'txtBuscarTelefono
+        '
+        Me.txtBuscarTelefono.Location = New System.Drawing.Point(102, 57)
+        Me.txtBuscarTelefono.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtBuscarTelefono.Name = "txtBuscarTelefono"
+        Me.txtBuscarTelefono.Size = New System.Drawing.Size(252, 19)
+        Me.txtBuscarTelefono.TabIndex = 14
+        '
+        'btnbuscar
+        '
+        Me.btnbuscar.BackColor = System.Drawing.Color.Transparent
+        Me.btnbuscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnbuscar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnbuscar.Location = New System.Drawing.Point(823, 16)
+        Me.btnbuscar.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnbuscar.Name = "btnbuscar"
+        Me.btnbuscar.Size = New System.Drawing.Size(39, 40)
+        Me.btnbuscar.TabIndex = 12
+        Me.btnbuscar.UseVisualStyleBackColor = False
+        '
+        'txtBuscarNombre
+        '
+        Me.txtBuscarNombre.Location = New System.Drawing.Point(434, 24)
+        Me.txtBuscarNombre.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtBuscarNombre.Name = "txtBuscarNombre"
+        Me.txtBuscarNombre.Size = New System.Drawing.Size(359, 19)
+        Me.txtBuscarNombre.TabIndex = 7
+        '
+        'txtBuscarID
+        '
+        Me.txtBuscarID.Location = New System.Drawing.Point(102, 24)
+        Me.txtBuscarID.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtBuscarID.Name = "txtBuscarID"
+        Me.txtBuscarID.Size = New System.Drawing.Size(252, 19)
+        Me.txtBuscarID.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(37, 60)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(61, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Teléfono:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(376, 27)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 27)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID Proveedor:"
         '
         'FrmProveedores
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(996, 595)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
         Me.Name = "FrmProveedores"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "INFO CREATIVOS - Proveedores"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents txtBuscarTelefono As TextBox
+    Friend WithEvents btnbuscar As Button
+    Friend WithEvents txtBuscarNombre As TextBox
+    Friend WithEvents txtBuscarID As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
