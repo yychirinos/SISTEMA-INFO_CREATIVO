c94193a152474803ae593b670788b5d5f59d8ecd
ninoska3005 <ninoska.moreno05@gmail.com>
2023-07-10 20:54:33 -0600

    Menu Principal (diseño y codigo)

---
+++ Sistema_Info_Creativo/Frm_MenuPrincipal.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_MenuPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AplicacionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReiniciarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MantenimientoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsuariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EmpleadosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProductosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProveedoresToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InventarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TransaccionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComprasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentasToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComprasToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ManualDeUsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'AplicacionToolStripMenuItem
+        '
+        Me.AplicacionToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ReiniciarToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.AplicacionToolStripMenuItem.Name = "AplicacionToolStripMenuItem"
+        Me.AplicacionToolStripMenuItem.Size = New System.Drawing.Size(93, 24)
+        Me.AplicacionToolStripMenuItem.Text = "Aplicacion"
+        '
+        'ReiniciarToolStripMenuItem
+        '
+        Me.ReiniciarToolStripMenuItem.Name = "ReiniciarToolStripMenuItem"
+        Me.ReiniciarToolStripMenuItem.Size = New System.Drawing.Size(149, 26)
+        Me.ReiniciarToolStripMenuItem.Text = "Reiniciar"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(149, 26)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'MantenimientoToolStripMenuItem
+        '
+        Me.MantenimientoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UsuariosToolStripMenuItem, Me.EmpleadosToolStripMenuItem, Me.ClientesToolStripMenuItem, Me.ProductosToolStripMenuItem, Me.ProveedoresToolStripMenuItem, Me.InventarioToolStripMenuItem})
+        Me.MantenimientoToolStripMenuItem.Name = "MantenimientoToolStripMenuItem"
+        Me.MantenimientoToolStripMenuItem.Size = New System.Drawing.Size(124, 24)
+        Me.MantenimientoToolStripMenuItem.Text = "Mantenimiento"
+        '
+        'UsuariosToolStripMenuItem
+        '
+        Me.UsuariosToolStripMenuItem.Name = "UsuariosToolStripMenuItem"
+        Me.UsuariosToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.UsuariosToolStripMenuItem.Text = "Usuarios"
+        '
+        'EmpleadosToolStripMenuItem
+        '
+        Me.EmpleadosToolStripMenuItem.Name = "EmpleadosToolStripMenuItem"
+        Me.EmpleadosToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.EmpleadosToolStripMenuItem.Text = "Empleados"
+        '
+        'ClientesToolStripMenuItem
+        '
+        Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.ClientesToolStripMenuItem.Text = "Clientes"
+        '
+        'ProductosToolStripMenuItem
+        '
+        Me.ProductosToolStripMenuItem.Name = "ProductosToolStripMenuItem"
+        Me.ProductosToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.ProductosToolStripMenuItem.Text = "Productos"
+        '
+        'ProveedoresToolStripMenuItem
+        '
+        Me.ProveedoresToolStripMenuItem.Name = "ProveedoresToolStripMenuItem"
+        Me.ProveedoresToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.ProveedoresToolStripMenuItem.Text = "Proveedores"
+        '
+        'InventarioToolStripMenuItem
+        '
+        Me.InventarioToolStripMenuItem.Name = "InventarioToolStripMenuItem"
+        Me.InventarioToolStripMenuItem.Size = New System.Drawing.Size(174, 26)
+        Me.InventarioToolStripMenuItem.Text = "Inventario"
+        '
+        'TransaccionesToolStripMenuItem
+        '
+        Me.TransaccionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VentasToolStripMenuItem, Me.ComprasToolStripMenuItem})
+        Me.TransaccionesToolStripMenuItem.Name = "TransaccionesToolStripMenuItem"
+        Me.TransaccionesToolStripMenuItem.Size = New System.Drawing.Size(114, 24)
+        Me.TransaccionesToolStripMenuItem.Text = "Transacciones"
+        '
+        'VentasToolStripMenuItem
+        '
+        Me.VentasToolStripMenuItem.Name = "VentasToolStripMenuItem"
+        Me.VentasToolStripMenuItem.Size = New System.Drawing.Size(151, 26)
+        Me.VentasToolStripMenuItem.Text = "Ventas"
+        '
+        'ComprasToolStripMenuItem
+        '
+        Me.ComprasToolStripMenuItem.Name = "ComprasToolStripMenuItem"
+        Me.ComprasToolStripMenuItem.Size = New System.Drawing.Size(151, 26)
+        Me.ComprasToolStripMenuItem.Text = "Compras"
+        '
+        'ReportesToolStripMenuItem
+        '
+        Me.ReportesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VentasToolStripMenuItem1, Me.ComprasToolStripMenuItem1})
+        Me.ReportesToolStripMenuItem.Name = "ReportesToolStripMenuItem"
+        Me.ReportesToolStripMenuItem.Size = New System.Drawing.Size(82, 24)
+        Me.ReportesToolStripMenuItem.Text = "Reportes"
+        '
+        'VentasToolStripMenuItem1
+        '
+        Me.VentasToolStripMenuItem1.Name = "VentasToolStripMenuItem1"
+        Me.VentasToolStripMenuItem1.Size = New System.Drawing.Size(151, 26)
+        Me.VentasToolStripMenuItem1.Text = "Ventas"
+        '
+        'ComprasToolStripMenuItem1
+        '
+        Me.ComprasToolStripMenuItem1.Name = "ComprasToolStripMenuItem1"
+        Me.ComprasToolStripMenuItem1.Size = New System.Drawing.Size(151, 26)
+        Me.ComprasToolStripMenuItem1.Text = "Compras"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ManualDeUsuarioToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(65, 24)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'ManualDeUsuarioToolStripMenuItem
+        '
+        Me.ManualDeUsuarioToolStripMenuItem.Name = "ManualDeUsuarioToolStripMenuItem"
+        Me.ManualDeUsuarioToolStripMenuItem.Size = New System.Drawing.Size(219, 26)
+        Me.ManualDeUsuarioToolStripMenuItem.Text = "Manual de Usuario."
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AplicacionToolStripMenuItem, Me.MantenimientoToolStripMenuItem, Me.TransaccionesToolStripMenuItem, Me.ReportesToolStripMenuItem, Me.AyudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(953, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Frm_MenuPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(953, 488)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Frm_MenuPrincipal"
+        Me.Text = "Frm_MenuPrincipal"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AplicacionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReiniciarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MantenimientoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UsuariosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EmpleadosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClientesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProductosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProveedoresToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InventarioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TransaccionesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ComprasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentasToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ComprasToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ManualDeUsuarioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+End Class
---
+++ Sistema_Info_Creativo/Frm_MenuPrincipal.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Frm_MenuPrincipal
+    Private Sub Frm_MenuPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ActivarMenu()
+
+    End Sub
+
+
+    Sub ActivarMenu()
+
+        Dim con As SqlClient.SqlConnection
+        Dim SQLcon As String
+
+        'Ocultar opciones principales
+        For Each opcion As ToolStripItem In MenuStrip1.Items
+            opcion.Visible = False
+
+        Next
+
+        'ocultar opciones desplegables
+
+        For Each opcion As ToolStripItem In AplicacionToolStripMenuItem.DropDownItems
+            opcion.Visible = False
+
+        Next
+
+        For Each opcion As ToolStripItem In MantenimientoToolStripMenuItem.DropDownItems
+            opcion.Visible = False
+
+        Next
+
+        For Each opcion As ToolStripItem In TransaccionesToolStripMenuItem.DropDownItems
+            opcion.Visible = False
+
+        Next
+
+        For Each opcion As ToolStripItem In ReportesToolStripMenuItem.DropDownItems
+            opcion.Visible = False
+
+        Next
+
+        For Each opcion As ToolStripItem In AyudaToolStripMenuItem.DropDownItems
+            opcion.Visible = False
+
+        Next
+
+        SQLcon = My.Settings.INFO_CREATIVO
+        con = New SqlClient.SqlConnection(SQLcon)
+
+        con.Open()
+        Dim cmd As SqlCommand = New SqlCommand("SELECT nivelacceso, opcion, descripcion FROM Menu_APP where nivelacceso = '" & VariablesPublicas.nivelacceso & "'", con)
+        cmd.CommandType = CommandType.Text
+        cmd.Parameters.AddWithValue("@bandera", 1)
+        cmd.Parameters.AddWithValue("@nivelacceso", VariablesPublicas.nivelacceso)
+
+
+        Using r = cmd.ExecuteReader
+            If r.HasRows Then
+                While (r.Read()) 'mientras el lector encuentre datos
+                    For Each opcion As ToolStripItem In MenuStrip1.Items
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+                            Exit For
+                        End If
+                    Next
+
+                    'HABILITAR MENU APLICACION
+                    For Each opcion As ToolStripItem In AplicacionToolStripMenuItem.DropDownItems
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+
+                        End If
+                    Next
+                    'HABILITAR MENU MANTENIMIENTO
+                    For Each opcion As ToolStripItem In MantenimientoToolStripMenuItem.DropDownItems
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+
+                        End If
+                    Next
+
+                    'HABILITAR MENU TRANSACCIONES
+                    For Each opcion As ToolStripItem In TransaccionesToolStripMenuItem.DropDownItems
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+
+                        End If
+                    Next
+                    'HABILITAR MENU REPORTES
+                    For Each opcion As ToolStripItem In ReportesToolStripMenuItem.DropDownItems
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+
+                        End If
+                    Next
+                    'HABILITAR MENU AYUDA
+                    For Each opcion As ToolStripItem In AyudaToolStripMenuItem.DropDownItems
+                        If r.GetString(2).Trim = opcion.Name.Trim Then
+                            opcion.Visible = True
+
+                        End If
+                    Next
+
+                End While
+
+            End If
+        End Using
+
+    End Sub
+
+
+End Class
+
